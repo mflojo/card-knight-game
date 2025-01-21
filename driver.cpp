6587eb916a223ee4dbb4873ef5ad3015517de950
mflojo <m.eric.flojo@gmail.com>
2025-01-20 17:22:42 -0800

    Add menu & user input for name

--- driver.cpp
+++ driver.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+void menu();
+
 int main() {
-    cout << "Let's get started!" << endl;
+    string username;
+
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << "  Welcome to Card Knight!" << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    cout << "\nPlease enter your username: ";
+    cin >> username;
+    cout << "Hello " << username << ", let's begin." << endl;
+}
+
+
+void menu() {
+    
 }
